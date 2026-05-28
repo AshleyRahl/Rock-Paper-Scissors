430afcc151b6440c913bf6213c5c8e0da73c7707
Ashley Rahl <asrahlcpt024@student.wethinkcode.co.za>
2026-05-28 13:28:59 +0200

    Added rulers and welcome mesaage

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,21 +8,40 @@ Rock vs scissor-> Rock wins
 paper vs scissor-> scissor wins.
 """
 import random
-choices = ["rock", "paper", "scissors"]
 
-user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+def play():
 
-computer_choice = random.choice(choices)
+    choices = ["rock", "paper", "scissors"]
 
-# Check if the choices are valid
-if user_choice not in choices:
-    print("Invalid choice. Please choose rock, paper, or scissors.")
+    # Welcome message and rules
+    print("\n--- Rock, Paper, Scissors ---\n")
+    print("Rules are simple:")
+    print("Rock smashes scissors.")
+    print("Paper covers rock.")
+    print("Scissors cut paper.\n")
+    print("Best out of 3 wins! Let's play!")
+    print("Type 'quit' to exit game.\n")
 
-if user_choice == computer_choice:
-    print("Its a tie")
-elif (user_choice == "rock" and computer_choice == "scissors") or \
-     (user_choice == "paper" and computer_choice == "rock") or \
-     (user_choice == "scissors" and computer_choice == "paper"):
-    print(f"You win! {user_choice.capitalize()} beats {computer_choice.capitalize()}.")
-else:
-    print(f"Computer wins! {computer_choice.capitalize()} beats {user_choice}.")
+    user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+    computer_choice = random.choice(choices)
+
+    # Check if the choices are valid
+    if user_choice not in choices:
+        return "Invalid choice. Please choose rock, paper, or scissors."
+
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
+
+def determine_winner(user_choice, computer_choice):
+    # Determine a Winner
+    if user_choice == computer_choice:
+        return "Its a tie"
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+        (user_choice == "paper" and computer_choice == "rock") or \
+        (user_choice == "scissors" and computer_choice == "paper"):
+        return f"You win! {user_choice.capitalize()} beats {computer_choice.capitalize()}."
+    else:
+        return f"Computer wins! {computer_choice.capitalize()} beats {user_choice}."
+
+if __name__ == "__main__":
+    play()
