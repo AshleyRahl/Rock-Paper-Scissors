383d8066aba6b11bd020c7bf09ff4c8e6cfdece2
Ashley Rahl <asrahlcpt024@student.wethinkcode.co.za>
2026-05-28 12:44:18 +0200

    Start new project! added input for 2 players, test its working

---
+++ rock_paper_scissors.py
@@ -0,0 +1,9 @@
+""""
+
+"""
+
+player1 = input("Player one, enter your choice (rock, paper, scissors): ").lower()
+
+player2 = input("Player two, enter your choice (rock, paper, scissors): ").lower()
+
+print(player1, player2)
